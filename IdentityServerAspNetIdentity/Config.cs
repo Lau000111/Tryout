fb4c0a6eeff090bb3cf3ac3c1e7eedd19c2ef990
Daniel Remetan <79636364+Remsiii@users.noreply.github.com>
2024-03-28 09:07:43 +0100

    schaum mal obs gewht

---
+++ IdentityServerAspNetIdentity/Config.cs
@@ -0,0 +1,84 @@
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
+using IdentityModel;
+
+namespace IdentityServerAspNetIdentity;
+
+public static class Config
+{
+    public static IEnumerable<IdentityResource> IdentityResources =>
+        new IdentityResource[]
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile(),
+            new IdentityResource()
+            {
+                Name = "verification",
+                UserClaims = new List<string> 
+                { 
+                    JwtClaimTypes.Email,
+                    JwtClaimTypes.EmailVerified
+                }
+            },
+            new IdentityResource("color", new [] { "favorite_color" })
+
+        };
+
+    public static IEnumerable<ApiScope> ApiScopes =>
+        new ApiScope[]
+        { 
+            new ApiScope(name: "api1", displayName: "My API")
+        };
+
+    public static IEnumerable<Client> Clients =>
+        new Client[] 
+        {
+            new Client
+            {
+                ClientId = "web",
+                ClientSecrets = { new Secret("secret".Sha256()) },
+
+                AllowedGrantTypes = GrantTypes.Code,
+                
+                // where to redirect to after login
+                RedirectUris = { "https://localhost:5002/signin-oidc" },
+
+                // where to redirect to after logout
+                PostLogoutRedirectUris = { "https://localhost:5002/signout-callback-oidc" },
+
+                AllowOfflineAccess = true,
+
+                AllowedScopes = new List<string>
+                {
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile,
+                    "verification",
+                    "api1",
+                    "color"
+                }
+            },
+            new Client
+            {
+                ClientId = "interactive.confidential",
+                ClientSecrets = { new Secret("secret".Sha256()) },
+
+                AllowedGrantTypes = GrantTypes.Code,
+                
+                // where to redirect to after login
+                RedirectUris = { "https://localhost:5173/signin-oidc" },
+
+                // where to redirect to after logout
+                PostLogoutRedirectUris = { "https://localhost:5173/signout-callback-oidc" },
+
+                AllowOfflineAccess = true,
+
+                AllowedScopes = new List<string>
+                {
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile,
+                    "verification",
+                    "api1",
+                }
+            }
+        };
+}
--- ProjectAlpha.AppHost/Program.cs
+++ ProjectAlpha.AppHost/Program.cs
@@ -1,11 +1,78 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Duende.Bff.Yarp;
+using React.Bff;
+using Microsoft.AspNetCore.Http;
+
+//Auth 2
+var builder2 = WebApplication.CreateBuilder(args);
+
+builder2.Services.AddBff()
+    .AddRemoteApis();
+
+builder2.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = "cookie";
+    options.DefaultChallengeScheme = "oidc";
+    options.DefaultSignOutScheme = "oidc";
+}).AddCookie("cookie", options =>
+{
+    options.Cookie.Name = "__Host-bff";
+    options.Cookie.SameSite = SameSiteMode.Strict;
+}).AddOpenIdConnect("oidc", options =>
+{
+    options.Authority = "https://localhost:5001/";
+    options.ClientId = "interactive.confidential";
+    options.ClientSecret = "secret";
+    options.ResponseType = "code";
+    options.ResponseMode = "query";
+
+    options.GetClaimsFromUserInfoEndpoint = true;
+    options.MapInboundClaims = false;
+    options.SaveTokens = true;
+
+    options.Scope.Clear();
+    options.Scope.Add("openid");
+    options.Scope.Add("profile");
+    options.Scope.Add("api1");
+    options.Scope.Add("offline_access");
+
+    options.TokenValidationParameters = new()
+    {
+        NameClaimType = "name",
+        RoleClaimType = "role"
+    };
+});
+
+builder2.Services.AddAuthorization();
+
+var app = builder2.Build();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseBff();
+app.UseAuthorization();
+app.MapBffManagementEndpoints();
+
+app.MapGroup("/todos")
+    .ToDoGroup()
+    .RequireAuthorization()
+    .AsBffApiEndpoint();
+
+app.MapFallbackToFile("/index.html");
+
+app.Run();
 
 var builder = DistributedApplication.CreateBuilder(args);
 
 // Backend
 var catalogProject = builder.AddProject<Projects.ProjectAlpha_Catalog>(nameof(Projects.ProjectAlpha_Catalog));
 var restaurantProject = builder.AddProject<Projects.ProjectAlpha_Restaurant>(nameof(Projects.ProjectAlpha_Restaurant));
- 
+
 var tableProject = builder.AddProject<Projects.ProjectAlpha_Table>(nameof(Projects.ProjectAlpha_Table));
 
 // Reverse-proxies
@@ -24,8 +91,6 @@ builder.AddNpmApp("admin-page", "../admin-page")
     .WithEndpoint(containerPort: 3001, scheme: "http", env: "PORT")
     .AsDockerfileInManifest();
 
-//Auth
-var builder2 = WebApplication.CreateBuilder(args);
 
 
 builder.Build().Run();
